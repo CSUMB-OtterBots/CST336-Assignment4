3b6670c010b0a120966729c2e8d9f3ff1105cce4
deep-Turquoise <nicholasnelson10@gmail.com>
2017-07-18 08:49:36 -0700

    css fix

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <title>Assignment 4: Team Project</title>
     <link href='//fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
-    <link rel="stylesheet" type="text/css" href="css/nav.css">
+    <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
     <h1>GOT HERE</h1>
