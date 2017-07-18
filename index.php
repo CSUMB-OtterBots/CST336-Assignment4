bb0477bf99f2ab3dca0406fb6037294efdfb3abb
deep-Turquoise <nicholasnelson10@gmail.com>
2017-07-18 08:42:37 -0700

    basics

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Assignment 4: Team Project</title>
+    <link href='//fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
+    <link rel="stylesheet" type="text/css" href="css/nav.css">
+</head>
+<body>
+    <h1>GOT HERE</h1>
+</body>
+<script>
+
+</script>
+</html>
