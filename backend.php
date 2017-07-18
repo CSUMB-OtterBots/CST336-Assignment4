0173284cbc492127a793cfacd08954e97d571ee6
deep-Turquoise <nicholasnelson10@gmail.com>
2017-07-18 11:08:37 -0700

    data dump

--- backend.php
+++ backend.php
@@ -1,1 +1,109 @@
 <?php
+
+function insertInitial() {
+    print "<h1>Here is the data: </h1>";
+
+    $conn = new mysqli("localhost", "root", "airpolo3", "CSTassignment4");
+
+    if ($conn->connect_error) { print "Database Connection Error"; }
+
+    $sql = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $sql2 = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $conn->query($sql);
+    $conn->query($sql2);
+
+    $sql = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $sql2 = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $conn->query($sql);
+    $conn->query($sql2);
+
+    $sql = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $sql2 = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $conn->query($sql);
+    $conn->query($sql2);
+
+    $sql = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $sql2 = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $conn->query($sql);
+    $conn->query($sql2);
+
+    $sql = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $sql2 = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $conn->query($sql);
+    $conn->query($sql2);
+
+    $sql = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $sql2 = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $conn->query($sql);
+    $conn->query($sql2);
+
+    $sql = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $sql2 = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $conn->query($sql);
+    $conn->query($sql2);
+
+    $sql = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $sql2 = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $conn->query($sql);
+    $conn->query($sql2);
+
+    $sql = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $sql2 = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $conn->query($sql);
+    $conn->query($sql2);
+
+    $sql = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $sql2 = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $conn->query($sql);
+    $conn->query($sql2);
+
+    $sql = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $sql2 = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $conn->query($sql);
+    $conn->query($sql2);
+
+    $sql = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $sql2 = "INSERT INTO tableA ('name', 'director', 'genre', 'rating') VALUES ('Starwars', 'John Williams', 'Space', '5')";
+    $conn->query($sql);
+    $conn->query($sql2);
+
+    $conn->close();
+}
+
+function getData() {
+    insertInitial();
+
+    print "<h1>Here is the data: </h1>";
+
+    $conn = new mysqli("localhost", "root", "airpolo3", "CSTassignment4");
+
+    if ($conn->connect_error) { print "Database Connection Error"; }
+
+    $sql = "SELECT * FROM tableA";
+    $sql2 = "SELECT * FROM tableb";
+
+    $sql = "SELECT * FROM tableA";
+    $sql2 = "SELECT * FROM tableb";
+
+    $result = $conn->query($sql);
+    $result2 = $conn->query($sql2);
+
+    print "<h2>Table A: </h2>";
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            print $row['title'] . "\t" . $row['director'] . "\t" . $row['genre'] . "\t" . $row['rating'];
+        }
+    }
+
+    print "<h2>Table B: </h2>";
+    if ($result2->num_rows > 0) {
+        while($row = $result2->fetch_assoc()) {
+            print $row['title'] . "\t" . $row['director'] . "\t" . $row['genre'] . "\t" . $row['rating'];
+        }
+    }
+    $conn->close();
+}
+
+$instructions = $_GET['instructions'];
+
+if($instructions == "getProjects") { getData(); }
--- index.php
+++ index.php
@@ -5,11 +5,26 @@
     <link href='//fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
-<body>
-    <div class="top_section"></div>
-    <div class="bottom_section"></div>
+<body onload="getRows();">
+    <div class="top_section">
+
+    </div>
+    <div id="bottom_section" class="bottom_section">
+
+    </div>
 </body>
 <script>
+    function getRows() {
+        if (window.XMLHttpRequest) { xmlhttp = new XMLHttpRequest(); }
+        else { xmlhttp = new ActiveXObject("Microsoft.XMLHTTP"); }
 
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("bottom_section").innerHTML = this.responseText;
+            }
+        }
+        xmlhttp.open("GET", "backend.php?instructions=getProjects", true);
+        xmlhttp.send();
+    }
 </script>
 </html>
