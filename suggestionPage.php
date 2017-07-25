10981104ec7ba42ab4beb8ea16646edee701ce03
nmauga1993 <nathanielmauga@gmail.com>
2017-07-25 11:09:44 -0400

    moreInfo() works
    gonna try and make it work with the button now

--- suggestionPage.php
+++ suggestionPage.php
@@ -28,34 +28,30 @@ function suggestMovie()
     $max = count($movieMatches) - 1;
     $randIndex = rand(0, $max);
     $movie = $movieMatches[$randIndex];
-    echo $movie['movieName'] . "<br />";
-
-//    global $movieMatches;
-//    $max = count($movieMatches) - 1;
-//    $randIndex = rand(0, $max);
-//    $suggestion = $movieMatches[$randIndex];
-//    return $suggestion;
+    return $movie;
 }
 
 function moreInfo($movie)
 {
-//    $sql = "SELECT MPAAId, rating
-//        FROM MPAA_ratings
-//        ORDER BY MPAAId ASC";
-//    $stmt = $dbconn -> prepare($sql);
-//    $stmt -> execute();
-//    $ratings = $stmt -> fetchAll();
-//
-//    $sql = "SELECT genreId, genre
-//        FROM movie_genres
-//        ORDER BY genreId ASC";
-//    $stmt = $dbconn -> prepare($sql);
-//    $stmt -> execute();
-//    $genres = $stmt -> fetchAll();
-    echo $movieInfo['movieDescription'] . "<br />";
-    echo $movie['imdbRating'] . "<br />";
-    echo $movie['rating'] . "<br />";
-    echo $movie['duration'] . "<br />";
+    global $dbconn;
+    $sql = "SELECT rating
+            FROM MPAA_ratings
+            WHERE MPAAId = :MPAAId";
+    $stmt = $dbconn -> prepare($sql);
+    $stmt -> execute(array (':MPAAId' => $movie['MPAAId']));
+    $rating = $stmt->fetch();
+
+    $sql = "SELECT movieDescription
+            FROM movie_descriptions
+            WHERE movieId = :movieId";
+    $stmt = $dbconn -> prepare($sql);
+    $stmt -> execute(array (':movieId' => $movie['movieId']));
+    $description = $stmt->fetch();
+
+    echo $rating['rating'] . "<br />";
+    echo "IMDb Rating " . $movie['imdbRating'] . "/10" . "<br />";
+    echo $description['movieDescription'] . "<br />";
+    echo $movie['duration'] . " min." . "<br />";
 }
 ?>
 <!DOCTYPE html>
@@ -67,10 +63,12 @@ function moreInfo($movie)
 <body>
 <h2>2016 Movie Suggestion Generator</h2>
 <?php
-suggestMovie();
+$movie = suggestMovie();
+echo $movie['movieName'] . "<br />";
+moreInfo($movie);
 ?>
 <form action="">
-<input type="button" value="more info" onclick="<?moreInfo()?>">
+<input type="button" value="more info" onclick="<?moreInfo($movie)?>">
 <input type="button" value="another suggestion" onclick="<? suggestMovie()?>">
 <input type="button" value="change parameters" onclick="movieHomepage.php">
 </form>
